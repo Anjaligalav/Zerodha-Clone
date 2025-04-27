c664938a72bb95b131142b8c5f3926d3a93c5fee
Anjaligalav <anjaligalav294@gmail.com>
2025-04-27 12:30:49 +0530

    make changes in hero page ofrontend

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Hero() {
     return ( 
@@ -8,7 +10,12 @@ function Hero() {
             </div>
             <h1 className='mt-5'>Invest in everything</h1>
             <p className='mb-5 fs-5 text-muted'>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-            <button className='btn btn-primary btn-lg mb-4'>Sign up now</button>
+            <Link 
+              to="/signup" 
+              className="btn btn-primary btn-lg mb-5"
+            >
+              Sign up
+            </Link>
         </div>
      );
 }
